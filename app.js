8315d4016bd554b5fa02dff85d36db9eabf4b50c
ZeAloTssss <ki7looov@gmail.com>
2020-02-20 16:52:56 +0300

    clear all

--- app.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("burger-btn").onclick = function () {
-    active()
-}
-
-function active() {
-    document.getElementById("headerNav__menu").classList.toggle("headerNav__menuActive")
-}
